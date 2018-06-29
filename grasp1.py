c6b02dfa16d8c98fcc6b1f5cda005e5b1e5e1835
gabriel <gabriel.stepien@inf.ufrgs.br>
2018-06-29 00:34:01 -0300

    grasp good

--- grasp1.py
+++ grasp1.py
@@ -24,7 +24,7 @@ FILE_TO_BEST_SOLUTION = sys.argv[1]
 MINUTES = int(sys.argv[2])
 NUM_VIZINHOS = 8
 SOLUCOES_GULOSAS = 100
-ALPHA = 0.1
+ALPHA = float(sys.argv[3])
 LOCAL_SEARCH_CRITERIA = 10
 NUM_CORES = 1
 
@@ -35,35 +35,29 @@ NUM_CORES = 1
 
 
 # -------- ----------GRASP --------- ---------------
-def grasp(d, l, inst, alpha):
+def grasp(d, l, inst):
+
+    M,L = d.shape
 
     start = cronometro.time()
 
     min_dist_m = calculate_min_distances(d)
+    min_dists_sorted = sorted(min_dist_m, key=itemgetter(1), reverse=True)[:]
 
-    #-------------------------------LOGS-----------------------------------------------------
-    logging.basicConfig(filename='results/'+str(datetime.datetime.now()),level=logging.DEBUG)
-    logging.info("Instância: {} ".format(inst))
-    #logging.info("GRASP ITERATIONS: {} ".format(interactions))
-    logging.info("SOLUCOES_GULOSAS: {} ".format(SOLUCOES_GULOSAS))
-    logging.info("ALPHA: {} ".format(alpha))
-    logging.info("LOCAL_SEARCH_CRITERIA: {} ".format(LOCAL_SEARCH_CRITERIA))
-    logging.info("NUM_VIZINHOS: {} ".format(NUM_VIZINHOS))
-    #-----------------------------------------------------------------------------------------
+
 
    
     f = 0
     S = []
     it = 0
     while (cronometro.time()-start)/60 < MINUTES:
-        s = greedy_randomized(d, l, alpha, min_dist_m)
-        # s = local_search(s, l, d)
+        s = greedy_randomized(d, l, min_dists_sorted)
+        s = local_search(s, l, d, min_dist_m)
         f_ = evaluate(s, d)
         if f_ > f:
             f = f_
             S[:] = s[:]
-            logging.info('{} - {} - {} - {}'.format(alpha, it, f, int(cronometro.time()-start)))
-        print("alpha: {} it. {}: bv: {} - seconds execution: {}...".format(alpha, it, f, int(cronometro.time()-start)))
+        print("it. {}: bv: {} - seconds execution: {}...".format(it, f, int(cronometro.time()-start)))
         it += 1
     logging.info("TOTAL TIME: {} ".format(int(cronometro.time()-start)))
 
@@ -73,24 +67,25 @@ def grasp(d, l, inst, alpha):
 # --------------------------------------------------
 # ----------- Início guloso Randomizado ------------
 
-def greedy_randomized(d, l, alpha, min_dist_m):
+def greedy_randomized(d, l, min_dist_m):
     """Gera solução inicial """
-
     M,L = d.shape
-   
-    index_max = int(math.floor(L*alpha))
-   
-    rlc = min_dist_m[:index_max]
-   
-    index_solution = random.randint(0, len(rlc)-1) 
-    best_sol = rlc[index_solution][1]
+    S = set()
+    cut = int(abs((len(min_dist_m)*ALPHA)))
+  
+    if l > cut:
+        print("Infactível, impossível criar conjunto de soluções.")
+        pass 
+    indexes = random.sample(range(1, cut), l)
+    for e in indexes:
+        S.add(min_dist_m[e][0])
 
-    return best_sol
+    return list(S)
 
 # ------------------------------------------------
 # ----------- Busca local  ------------
 
-def local_search(S, l, d):
+def local_search(S, l, d, min_dists):
     """ Aplica uma busca local com NUM_VIZINHOS vizinhos """
 
     _, L = d.shape
@@ -98,51 +93,64 @@ def local_search(S, l, d):
     best_sol = []
     best_sol_value = 0
 
-    for it in range(LOCAL_SEARCH_CRITERIA):
+    S_  = S[:]
+    f = evaluate(S_,d)
 
-        S_ = S[:]
-        f = evaluate(S_,d)
+    # GERA OS VIZINHOS E SEUS VALORES (S,VAL) 
+    N = generate_neighbors(S_, L, d, min_dists)
+    best_index = np.argmax([x[1] for x in N])
+    best_neighbor = N[best_index][0]
+    bn_value = N[best_index][1]
 
-        N = generate_neighbors(S_, L)
-        best_neighbor = N[np.argmax([evaluate(x, d) for x in N])]
-        bn_value = evaluate(best_neighbor, d)
+    while bn_value > f:
 
-        while bn_value > f:
-    
-            f = bn_value
-            S_[:] = best_neighbor[:]
+        f = bn_value
+        S_[:] = best_neighbor[0][:]
 
-            N[:] = generate_neighbors(S_, L)
-            best_neighbor[:] = N[np.argmax([evaluate(x, d) for x in N])].copy()
-            
-            bn_value = evaluate(best_neighbor, d)
-        if f > best_sol_value:
-            best_sol[:] = S_[:]
-            best_sol_value = f
-
-    return best_sol
+        N = generate_neighbors(S_, L, d, min_dists)
+        best_index = np.argmax([x[1] for x in N])
+        best_neighbor = N[best_index][0]
+        bn_value = N[best_index][1]
 
 
 
-
+    return N[best_index][0]
 
 
 # ----------- Geração dos vizinhos   -------------
 
-def generate_neighbors(S,L):
+def generate_neighbors(S,L,d,min_dists):
 
-    S_ = S[:]
+    s_value = evaluate(S,d)
+
+    l = len(S)
+    possible = set(range(L)) - set(S)
+    possibles = list(possible)
     
     N = []
-    while len(N) < NUM_VIZINHOS:
-        index = random.randint(0,len(S_)-1)
-        n = random.randint(0,L-1)
-        while n in S_:
-            n = random.randint(0,L-1)
-        if (n >= 450):
-            break
-        S_[index] = n
-        N.append(S_)
+    for i in range(l):
+        for new_element in possibles:
+            
+            S_ = S[:]
+            S_[i] = new_element
+   
+            n_value = min(s_value, get_element_min_dist(new_element, min_dists))
+            N.append(tuple([S_, n_value]))
+    
+
+
+
+    
+    # N = []
+    # while len(N) < NUM_VIZINHOS:
+    #     index = random.randint(0,len(S_)-1)
+    #     n = random.randint(0,L-1)
+    #     while n in S_:
+    #         n = random.randint(0,L-1)
+    #     if (n >= 450):
+    #         break
+    #     S_[index] = n
+    #     N.append(S_)
     return N 
 
 
@@ -151,6 +159,7 @@ def generate_neighbors(S,L):
 
 def evaluate(S, d):
     """Função objetivo"""
+  
     M, L = d.shape
     result = 0
     for m in range(M):
@@ -160,22 +169,24 @@ def evaluate(S, d):
         result += dm
     return result
 
-
 # -----------------------------------------------
-# ------------- Cálculo das distâncias mínimas
-
 def calculate_min_distances(d):
     """ Calcula as distâncias mínimas e retorna uma lista com (l,dist) ordanada """
     M, L = d.shape
     min_dists_vs_l = []
 
     for l in range(L):
-        min_dist = math.inf
+        min_dist = np.inf
         for m in range(M):
             min_dist = min(min_dist, d[m,l])
-        min_dists_vs_l.append((l, min_dist))
+        min_dists_vs_l.append(tuple([l, min_dist]))   
     
-    return sorted(min_dists_vs_l, key=itemgetter(0), reverse=True)
+    return min_dists_vs_l
+
+# -------------------------------------------------
+
+def get_element_min_dist(e, min_dists):
+    return min_dists[e][1]
 
 # ------------------------------------------------
 # ----------- Parser para a entrada  ------------
@@ -207,11 +218,15 @@ def parse_instance(inst):
             
 def main():
     
-    instancia = input()
+    #instancia = input()
+    instancia = '/home/gabriel/git/biggest-min-dist-selection-grasp/instances/mdmt39.112.A.ins'
     M, L, l, d = parse_instance(instancia)
-    #S, value = grasp(d,l,instancia,ALPHA)
-    print(calculate_min_distances(d))
-    
+
+    if (ALPHA*L) < l:
+        print("impossível prosseguir para esse valor de alpha")
+    else:
+        S, value = grasp(d,l,instancia)
+  
     f=open(FILE_TO_BEST_SOLUTION, 'w')
     f.write("Best value: \n")
     f.write(str(value))
